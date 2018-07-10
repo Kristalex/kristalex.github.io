b57e471f4f2c552bd22c5f7a33ee93729b2eb184
Kristalex <kris.alexeeva@gmail.com>
2018-07-10 16:08:51 -0700

    no cesar

--- cesar/includes/contact.php
+++
@@ -1,9 +0,0 @@
-<div class="content content__grey center py-5">
-    <img class="mt-5" src="img/cesar-min.png" width="136" height="136" alt="Cesar Zeigler">
-    <h3 class="g-h3 pt-2">Like my work?</h3>
-    <p class="g-text-medium">I'm happy to create something wonderful for&nbsp;you.</p>
-    <div class="buttons pb-5 mb-2"><div class="buttons_in">
-        <a class="button" href="pricing.php">Pricing</a>
-        <a class="button" href="contacts.php">Contact</a>
-    </div></div>
-</div>
--- cesar/includes/footer.php
+++
@@ -1,3 +0,0 @@
-<div class="footer topBorder center py-4">
-	© 2018 Cexarmusic
-</div>
